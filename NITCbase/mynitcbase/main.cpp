ffdc28d5b01f3cecad7bd0afbd94e1df562e0113
shalinv <shalinvenadgit@gmail.com>
2026-01-08 12:07:24 +0530

    Stage 1 Completed

--- NITCbase/mynitcbase/main.cpp
+++ NITCbase/mynitcbase/main.cpp
@@ -3,11 +3,42 @@
 #include "Disk_Class/Disk.h"
 #include "FrontendInterface/FrontendInterface.h"
 
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
 int main(int argc, char *argv[]) {
   /* Initialize the Run Copy of Disk */
   Disk disk_run;
+
   // StaticBuffer buffer;
   // OpenRelTable cache;
 
-  return FrontendInterface::handleFrontend(argc, argv);
+  //write
+  unsigned char buffer[BLOCK_SIZE];
+  Disk::readBlock(buffer,7000);
+  char message[] = "hello";
+  memcpy(buffer+20, message, 6);
+  Disk::writeBlock(buffer, 7000);
+
+  //read
+  unsigned char buffer2[BLOCK_SIZE];
+  char message2[6];
+  Disk::readBlock(buffer2, 7000);
+  memcpy(message2, buffer2+20, 6);
+  cout << message2 << '\n';
+
+  //read the first block
+
+  unsigned char buffer3[BLOCK_SIZE];
+  Disk::readBlock(buffer3, 0);
+  for(int i=0; i<6; i++){
+    cout << (int)buffer3[i] << " ";
+  }
+  cout << "\n";
+
+  return 0;
+
+  //return FrontendInterface::handleFrontend(argc, argv);
 }
