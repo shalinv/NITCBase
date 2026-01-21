c853d3706a9ac05bae59dd5811ac669db835623e
shalinv <shalinvenadgit@gmail.com>
2026-01-21 12:01:28 +0530

    Created disk buffer

--- NITCbase/mynitcbase/Buffer/BlockBuffer.cpp
+++ NITCbase/mynitcbase/Buffer/BlockBuffer.cpp
@@ -14,16 +14,41 @@ RecBuffer::RecBuffer(int blockNum) : BlockBuffer::BlockBuffer(blockNum) {}
 
 // load the block header into the argument pointer
 int BlockBuffer::getHeader(struct HeadInfo *head) {
-  unsigned char buffer[BLOCK_SIZE];
+  
+  unsigned char *bufferPtr;
+  int ret = loadBlockAndGetBufferPtr(&bufferPtr);
+  if (ret != SUCCESS) {
+    return ret;   // return any errors that might have occured in the process
+  }
 
   // read the block at this.blockNum into the buffer
-  Disk::readBlock(buffer, this->blockNum);
+  Disk::readBlock(bufferPtr, this->blockNum);
   // populate the numEntries, numAttrs and numSlots fields in *head
-  memcpy(&head->numSlots, buffer + 24, 4);
-  memcpy(&head->numEntries, buffer + 16, 4);
-  memcpy(&head->numAttrs, buffer + 20, 4);
-  memcpy(&head->rblock, buffer + 12, 4);
-  memcpy(&head->lblock, buffer + 8, 4);
+  memcpy(&head->numSlots, bufferPtr + 24, 4);
+  memcpy(&head->numEntries, bufferPtr + 16, 4);
+  memcpy(&head->numAttrs, bufferPtr + 20, 4);
+  memcpy(&head->rblock, bufferPtr + 12, 4);
+  memcpy(&head->lblock, bufferPtr + 8, 4);
+
+  return SUCCESS;
+}
+
+int BlockBuffer::loadBlockAndGetBufferPtr(unsigned char **buffPtr) {
+  // check whether the block is already present in the buffer using StaticBuffer.getBufferNum()
+  int bufferNum = StaticBuffer::getBufferNum(this->blockNum);
+
+  if (bufferNum == E_BLOCKNOTINBUFFER) {
+    bufferNum = StaticBuffer::getFreeBuffer(this->blockNum);
+
+    if (bufferNum == E_OUTOFBOUND) {
+      return E_OUTOFBOUND;
+    }
+
+    Disk::readBlock(StaticBuffer::blocks[bufferNum], this->blockNum);
+  }
+
+  // store the pointer to this buffer (blocks[bufferNum]) in *buffPtr
+  *buffPtr = StaticBuffer::blocks[bufferNum];
 
   return SUCCESS;
 }
--- NITCbase/mynitcbase/main.cpp
+++ NITCbase/mynitcbase/main.cpp
@@ -2,72 +2,78 @@
 #include "Cache/OpenRelTable.h"
 #include "Disk_Class/Disk.h"
 #include "FrontendInterface/FrontendInterface.h"
-
 #include <iostream>
-#include <cstring>
-
-using namespace std;
 
 int main(int argc, char *argv[]) {
   Disk disk_run;
-
-  int attrBlockNum=ATTRCAT_BLOCK;
-  while(attrBlockNum!=-1)
-  {
+  StaticBuffer buffer;
+  
+  int attrBlockNum = ATTRCAT_BLOCK;
+  while (attrBlockNum != -1) {
     RecBuffer attrCatBuffer(attrBlockNum);
     HeadInfo attrCatHeader;
     attrCatBuffer.getHeader(&attrCatHeader);
-    for(int j=0;j<attrCatHeader.numEntries;j++)
-    {
-      // declare attrCatRecord and load the attribute catalog entry into it
+
+    for (int j = 0; j < attrCatHeader.numEntries; j++) {
       Attribute attrCatRecord[ATTRCAT_NO_ATTRS];
-      attrCatBuffer.getRecord(attrCatRecord,j);
-      if(strcmp(attrCatRecord[ATTRCAT_REL_NAME_INDEX].sVal,"Students")==0 && strcmp(attrCatRecord[ATTRCAT_ATTR_NAME_INDEX].sVal,"Class")==0)
-      {
-        strcpy(attrCatRecord[ATTRCAT_ATTR_NAME_INDEX].sVal,"Batch");
+      attrCatBuffer.getRecord(attrCatRecord, j);
+
+      if (!strcmp(attrCatRecord[ATTRCAT_REL_NAME_INDEX].sVal, "Students") &&
+          !strcmp(attrCatRecord[ATTRCAT_ATTR_NAME_INDEX].sVal, "Mark")) {
+
+        strcpy(attrCatRecord[ATTRCAT_ATTR_NAME_INDEX].sVal, "Grade");
+
         unsigned char buffer[BLOCK_SIZE];
-        Disk::readBlock(buffer,attrBlockNum);
-        int recSize=attrCatHeader.numAttrs*ATTR_SIZE;
-        int offset=HEADER_SIZE+SLOTMAP_SIZE_RELCAT_ATTRCAT+(recSize*j);
-        memcpy(buffer+offset,attrCatRecord,recSize);
-        Disk::writeBlock(buffer,attrBlockNum);
+        Disk::readBlock(buffer, attrBlockNum);
+
+        int recSize = attrCatHeader.numAttrs * ATTR_SIZE;
+        int offset = HEADER_SIZE + SLOTMAP_SIZE_RELCAT_ATTRCAT + (recSize * j);
+
+        memcpy(buffer + offset, attrCatRecord, recSize);
+        Disk::writeBlock(buffer, attrBlockNum);
       }
     }
-    attrBlockNum=attrCatHeader.rblock;
+    attrBlockNum = attrCatHeader.rblock;
   }
 
-  // create objects for the relation catalog and attribute catalog
-  RecBuffer relCatBuffer(RELCAT_BLOCK);
-
-  HeadInfo relCatHeader;
   
-  // load the headers of both the blocks into relCatHeader and attrCatHeader.
+  RecBuffer relCatBuffer(RELCAT_BLOCK);
+  HeadInfo relCatHeader;
   relCatBuffer.getHeader(&relCatHeader);
-  for(int i=0;i<relCatHeader.numEntries;i++)
-  {
-    Attribute relCatRecord[RELCAT_NO_ATTRS];// will store the record from the relation catalog
-    relCatBuffer.getRecord(relCatRecord,i);
-    printf("Relation: %s\n",relCatRecord[RELCAT_REL_NAME_INDEX].sVal);
-    int attrBlockNum=ATTRCAT_BLOCK;
-    while(attrBlockNum!=-1)
-    {
+
+  for (int i = 0; i < relCatHeader.numEntries; i++) {
+    Attribute relCatRecord[RELCAT_NO_ATTRS];
+    relCatBuffer.getRecord(relCatRecord, i);
+
+    printf("Relation: %s\n", relCatRecord[RELCAT_REL_NAME_INDEX].sVal);
+
+    int attrBlockNum = ATTRCAT_BLOCK;
+    while (attrBlockNum != -1) {
       RecBuffer attrCatBuffer(attrBlockNum);
       HeadInfo attrCatHeader;
       attrCatBuffer.getHeader(&attrCatHeader);
-      for(int j=0;j<attrCatHeader.numEntries;j++)
-      {
-        // declare attrCatRecord and load the attribute catalog entry into it
+
+      for (int j = 0; j < attrCatHeader.numEntries; j++) {
         Attribute attrCatRecord[ATTRCAT_NO_ATTRS];
-        attrCatBuffer.getRecord(attrCatRecord,j);
-        if(strcmp(relCatRecord[RELCAT_REL_NAME_INDEX].sVal,attrCatRecord[ATTRCAT_REL_NAME_INDEX].sVal)==0)
-        {
-          const char*attrType=attrCatRecord[ATTRCAT_ATTR_TYPE_INDEX].nVal==NUMBER ? "NUM" : "STR";
-          printf("  %s: %s\n",attrCatRecord[ATTRCAT_ATTR_NAME_INDEX].sVal,attrType);
+        attrCatBuffer.getRecord(attrCatRecord, j);
+
+        if (!strcmp(relCatRecord[RELCAT_REL_NAME_INDEX].sVal,
+                     attrCatRecord[ATTRCAT_REL_NAME_INDEX].sVal)) {
+
+          const char *attrType =
+              attrCatRecord[ATTRCAT_ATTR_TYPE_INDEX].nVal == NUMBER ? "NUM" : "STR";
+
+          printf("  %s: %s\n",
+                 attrCatRecord[ATTRCAT_ATTR_NAME_INDEX].sVal,
+                 attrType);
         }
       }
-      attrBlockNum=attrCatHeader.rblock;
+      attrBlockNum = attrCatHeader.rblock;
     }
+
+    
     printf("\n");
   }
+
   return 0;
 }
